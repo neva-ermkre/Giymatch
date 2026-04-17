c7fc92cf264a079b99a0e0259cd9ec16f7ba0c1e
neva <neva.eremkere@ogr.gelisim.edu.tr>
2026-04-17 16:38:42 +0300

    Add button click handler and imports in controller

--- AnaekranController.java
+++ AnaekranController.java
@@ -1,1 +1,13 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class AnaEkranController {
+
+    @FXML
+    private Button ekleButonu;
+
+    @FXML
+    void ekleButonunaBasildi() {
+        System.out.println("Butona basıldı! Veritabanı bağlantısı bekleniyor...");
+    }
+}
