2fff521bb166ee7ab1fc2b4f94cab06075060743
Semanur Bulut <semanur.bulut@ogr.gelisim.edu.tr>
2026-04-24 19:50:20 +0300

    Add Kiyafet class with properties and constructor

---
+++ Kiyafet.java
@@ -0,0 +1,14 @@
+public class Kiyafet {
+	String ad;
+    String tur;   
+    String renk;
+    String mevsim; 
+
+    public Kiyafet(String ad, String tur, String renk, String mevsim) {
+        this.ad = ad;
+        this.tur = tur;
+        this.renk = renk;
+        this.mevsim = mevsim;
+    }
+}
+
