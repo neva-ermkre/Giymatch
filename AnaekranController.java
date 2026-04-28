d23a12e681a51db84a2e285f7dfd6940c25ca870
neva <neva.eremkere@ogr.gelisim.edu.tr>
2026-04-28 17:17:54 +0300

    Modify ekleButonunaBasildi to add clothing to database
    Updated ekleButonunaBasildi method to include database interaction for adding a new clothing item.

--- AnaekranController.java
+++ AnaekranController.java
@@ -1,3 +1,4 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
@@ -7,7 +8,18 @@ public class AnaEkranController {
     private Button ekleButonu;
 
     @FXML
-    void ekleButonunaBasildi() {
-        System.out.println("Butona basıldı! Veritabanı bağlantısı bekleniyor...");
+    void ekleButonunaBasildi(ActionEvent event) {
+        //  Konsola bilgi veriyoruz
+        System.out.println("Butona basıldı! Veritabanına kayıt ekleniyor...");
+
+        try {
+           
+            Kiyafet yeniKiyafet = new Kiyafet(0, "Mavi Tişört", "Üst Giyim", "Mavi", "Spor");
+            VeritabaniBaglantisi.kiyafetEkle(yeniKiyafet);
+
+            System.out.println("Başarılı: Mavi Tişört veritabanına kaydedildi!");
+        } catch (Exception e) {
+            System.err.println("Kayıt sırasında bir hata oluştu: " + e.getMessage());
+        }
     }
 }
